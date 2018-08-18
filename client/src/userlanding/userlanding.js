2abd8e592a9b5129bf2dd6a19fefcdded91031b9
Ken Church <30963709+kchurch9@users.noreply.github.com>
2018-08-18 10:45:33 -0600

    added login files

---
+++ client/src/userlanding/userlanding.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export default class App extends React.Component {
+  render() {
+    return (
+          <div id="board"> 
+            <table>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+          </div>
+    )
+  }
+}
