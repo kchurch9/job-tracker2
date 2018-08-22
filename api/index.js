0263e3f478673886de88eff5305cc5155652cbed
Ken Church <30963709+kchurch9@users.noreply.github.com>
2018-08-21 22:46:24 -0600

    loggin .state changes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express') //imports
+const cors = require('cors') //imports
+const bodyParser = require('body-parser') //imports
 
-app.use(cors())
+const app = express() //Creates an Express application. The express() function is a top-level function exported by the express module.
 
-app.post('/', (req, res) => res.send('hello world!'))
+app.use(cors()) //layers of express middleware 
+app.use(bodyParser.json())//deserializes request.body(req) from to json to object
 
+const users = [
+    {email: 'bob', password:'1234'},
+    {email:'hanes',password:'123'}
+]
+
+app.post('/login', handleLoginRequest)//
+function handleLoginRequest(req, res) { 
+    const typedEmail =req.body.email
+    const typedPassword = req.body.password //this is an example of a variable you stupid fucker
+    
+    let isCorrectLogin =false
+    for (let i = 0; i < users.length; i++){
+        const dbuser = users[i]
+        if (dbuser.email === typedEmail && dbuser.password ===typedPassword){
+            isCorrectLogin=true
+        }
+    }
+    if (isCorrectLogin){
+        res.sendStatus(200)
+    } 
+    else{
+        res.sendStatus(401)
+    }
+}
 
 app.listen(4001, () => console.log('example app listening on port 4001'))
--- client/src/app.js
+++ client/src/app.js
@@ -4,15 +4,24 @@ import axios from 'axios'
 export default class App extends React.Component {
   constructor(props){
     super(props)
-    this.state = {email: '' , password: ''}
-  }
+    this.state = {email: '' , password: '', isSuccess: false, hasTriedToLogIn: false }
+    }
   handleClick = () => {
-    const url = 'http://localhost:4001'
+    const url = 'http://localhost:4001/login'
     const data = {
       email: this.state.email,
       password: this.state.password
     }
     axios.post(url,data)
+      .then(res =>{
+        console.log('status:',res.status)
+        this.setState({isSuccess:true, hasTriedToLogIn:true})
+      })
+      .catch(err=>{
+        console.log('error:',err.response.status)
+        this.setState({isSuccess:false, hasTriedToLogIn:true})
+      })
+      
   }
   handleEmailChange = (event) => {
     this.setState({ email: event.target.value })
@@ -21,9 +30,19 @@ export default class App extends React.Component {
     this.setState({ password: event.target.value })
   }
   render() {
+    let logginMessage=''
+    if (this.state.hasTriedToLogIn){
+      if(this.state.isSuccess){
+        logginMessage='Granted'
+      } else {
+        logginMessage="Denied"
+      }
+    }
+
     return (
       <div>
         <form>
+          <div>{logginMessage}</div> 
           <div><input type="text" onChange={this.handleEmailChange} placeholder="Email"/></div>
           <div><input type="password" onChange={this.handlePasswordChange} placeholder="Password"/></div>
           <div> <button onClick={this.handleClick} type="button">Click Here </button></div>
@@ -32,5 +51,3 @@ export default class App extends React.Component {
     )
   }
 }
-
-
